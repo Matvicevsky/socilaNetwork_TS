858a58aa086d3090b2c90d1fdc16e37c10533272
Matvicevsky <matvicevsky_ilya@mail.ru>
2021-03-10 12:32:22 +0300

    add Header, Navbar and Profile

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <div>
+            <img className={"logo"}
+                 src={"https://seeklogo.com/images/M/michigan-wolverines-logo-9E0DB2CF1E-seeklogo.com.png"}
+                 alt={"logo"}/>
+        </div>
+    );
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export function Profile() {
+    return (
+        <div>
+            <img className={"title-png"} src="https://cdn1.ftimg.com/images/logos/big/en_US/matrix-logo.png"
+                 alt="png"/>
+            <div>
+                <img className={"avatar"}
+                     src={"https://cdn1.iconfinder.com/data/icons/diversity-avatars-volume-01-circles/64/matrix-neo-man-white-512.png"}/>
+                ava+info
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+            </div>
+            <div>
+                posts
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    );
+}
